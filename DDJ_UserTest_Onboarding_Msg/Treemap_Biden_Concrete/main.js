ca40cf664aa444a410d657e94b4dd9cb997151c2
doomsayer2 <fgrassinger@gmx.at>
2022-01-16 17:17:17 +0100

    Adds dynamic titles and text size to treemap biden concrete (refs #2).

--- DDJ_UserTest_Onboarding_Msg/Treemap_Biden_Concrete/main.js
+++ DDJ_UserTest_Onboarding_Msg/Treemap_Biden_Concrete/main.js
@@ -1,4 +1,4 @@
-import { createTreemap } from './src/treemap';
+import { createTreemap, clearTreemap } from './src/treemap';
 import { type } from '@antv/dw-analyzer';
 // import data from './src/data/sample.json';
 import dataJobs from './src/data/usaJobsPlan.json';
@@ -10,18 +10,25 @@ import './style.css';
  * Treemaps
  */
 
-// Sample Treemap
-// createTreemap('#chart', data);
+const charts = () => {
+    // Chart width and heights
+    const width = (window.innerWidth / 2) - 20;
+    const height = (window.innerHeight * 0.9);
+    
+    // Jobs Plan Treemap
+    createTreemap('#chart', dataJobs, undefined, { width, height });
+    
+    // Families Plan Treemap
+    createTreemap('#chart2', dataFamilies, undefined, { width, height });
+};
 
-// Chart width and heights
-const width = (window.innerWidth / 2) - 20;
-const height = (window.innerHeight * 0.9);
+charts();
 
-// Jobs Plan Treemap
-createTreemap('#chart', dataJobs, undefined, { width, height });
-
-// Families Plan Treemap
-createTreemap('#chart2', dataFamilies, undefined, { width, height });
+window.onresize = () => {
+    clearTreemap('#chart');
+    clearTreemap('#chart2');
+    charts();
+};
 
 /**
  * Data Analysis
