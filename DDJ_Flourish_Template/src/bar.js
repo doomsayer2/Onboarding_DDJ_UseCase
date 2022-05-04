68d66244dbc4cfd5ef3c27c2ba32c5041165939b
doomsayer2 <fgrassinger@gmx.at>
2022-05-04 14:00:45 +0200

    Testing visahoi.

--- DDJ_Flourish_Template/src/bar.js
+++ DDJ_Flourish_Template/src/bar.js
@@ -3,6 +3,7 @@ import {
   ahoi,
   generateBasicAnnotations,
 } from '../static/lib/bundle.js';
+import * as d3 from 'd3';
 
 let chart = null;
 let showOnboarding = false;
@@ -56,13 +57,46 @@ export default class Bar {
     if(!helpIcon) { return; }
     helpIcon.addEventListener('click', async () => {
       if(showOnboarding) {
-        const defaultOnboardingMessages = generateBasicAnnotations(EVisualizationType.BAR_CHART, chart);
-        const extendedOnboardingMessages = defaultOnboardingMessages.map((d) => ({
-          ...d,
-          text: "test123"
-        }));
+        const defaultOnboardingMessages = generateBasicAnnotations(EVisualizationType.BAR_CHART, chart);        
+
+        const extendedOnboardingMessages = [...defaultOnboardingMessages];
+        extendedOnboardingMessages.push(
+          {
+            "anchor": {
+                "element": d3.select('text.gtitle').node(),
+                "findDomNodeByValue": true,
+                "offset": {
+                    "left": -40,
+                    "top": 20
+                }
+            },
+            "requires": [
+                "chartTitle"
+            ],
+            "text": "Just use it!",
+            "title": "Using the chart ",
+            "onboardingStage": {
+                "id": "flo-the-chart",
+                "title": "Chart has a long title over 2 lines",
+                "iconClass": "fas fa-arrows",
+                "hoverBackgroundColor": "rgb(122, 34, 112)",
+                "backgroundColor": "rgb(45, 23, 150)",
+                "activeBackgroundColor": "rgb(134, 46, 94)",
+                "order": 3
+            },
+            "marker": {
+                "id": "flo-marker",
+                "radius": 50,
+                "fontSize": "40pt",
+                "content": "X"
+            }
+        }
+        );
+
+        console.log({extendedOnboardingMessages});
+
         const ahoiConfig = {
-          onboardingMessages: y,
+          onboardingMessages: extendedOnboardingMessages,
         }
         onboardingUI = await ahoi(EVisualizationType.BAR_CHART, chart, ahoiConfig);
       } else {
